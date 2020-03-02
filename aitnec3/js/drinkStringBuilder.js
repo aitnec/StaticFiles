5608657d80727fbde5b8d48f9e27208df1f0fa21
service <service@service.service>
2020-03-02 10:59:19 +0000

    updatewwwroot

---
+++ aitnec3/js/drinkStringBuilder.js
@@ -0,0 +1,38 @@
+﻿
+
+$(function () {
+    
+    $('.radioWrapper input, .fa-minus, .fa-plus').on('click', (e) => {
+        var url = new URL(window.location.href);
+        var c = url.searchParams.get("categoryId");
+        console.log(c);
+
+        var order = "Drink?";
+        order += "categoryId=" + c + "&order=";
+
+        order += $("#amount")[0].value;
+        order += " ";
+        
+
+        //Get Name
+        $.each($('.drinkID'), (i, el) => {
+                order += el.id;
+        });
+
+        order += " ";
+
+        //Get Size
+        $.each($('.active'), (i, el) => {
+            order += el.id;
+        });
+
+
+
+        
+        window.history.replaceState("sth", "Title", order);
+        //console.log(order);
+    });
+
+
+});
+
