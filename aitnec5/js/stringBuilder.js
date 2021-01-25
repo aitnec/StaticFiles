dea9c9644ed21e88afe0893daadc70b79f175f25
service <service@service.service>
2021-01-25 22:06:44 +0000

    updatewwwroot

--- aitnec5/js/stringBuilder.js
+++ aitnec5/js/stringBuilder.js
@@ -1,14 +1,18 @@
 ﻿
 
 $(function () {
-    
-    $('.radioWrapper input').on('click', (e) => {
+/*yi modified in Jan. 2021 food also supports multi portions*/
+    $('.radioWrapper input, .fa-minus, .fa-plus').on('click', (e) => {
         var url = new URL(window.location.href);
         var c = url.searchParams.get("categoryId");
         console.log(c);
 
         var order = "Food?";
         order += "categoryId=" + c + "&order=";
+
+        
+        order += $("#amount")[0].value;
+        order += " ";
 
         $.each($('.foodID'), (i, el) => {
                 order += el.id;
